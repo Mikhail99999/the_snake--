bae4212f44a7eb8b31dfb6e8dd5b1d95df0d1c7f
Mikhail99999 <Mikhail.Anokhin98@yandex.ru>
2024-08-06 17:40:21 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -1,4 +1,4 @@
-from random import choice, randint
+from random import choice, randrange
 
 import pygame
 
@@ -14,25 +14,18 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Цвет фона - черный:
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 
-# Цвет границы ячейки
 BORDER_COLOR = (93, 216, 228)
 
-# Цвет яблока
 APPLE_COLOR = (255, 0, 0)
 
-# Цвет змейки
 SNAKE_COLOR = (0, 255, 0)
 
-# Цвет апельсина
 ORANGE_COLOR = (253, 165, 15)
 
-# Цвет черники
 BLUEBERRY_COLOR = (79, 135, 247)
 
-# Цвет камня
 STONE_COLOR = (173, 165, 135)
 
 # Скорость движения змейки:
@@ -48,32 +41,36 @@ pygame.display.set_caption('Змейка')
 clock = pygame.time.Clock()
 
 
-# Тут опишите все классы игры.
 class GameObject():
+    """Описание начального класса"""
+
     def __init__(self):
         self.position = ((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))
         self.body_color = None
 
     def draw(self):
-        pass
-
-
-class Apple(GameObject):
-    def __init__(self):
-        super().__init__()
-        self.body_color = APPLE_COLOR
-
-    def randomize_position(self):
-        self.position = (randint(1, 31) * GRID_SIZE,
-                         randint(1, 23) * GRID_SIZE)
-
-    def draw(self):
+        """Метод прорисовки для объекта"""
         rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(screen, self.body_color, rect)
         pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
+    def randomize_position(self):
+        """Метод определения случайного положения объекта"""
+        self.position = (randrange(20, 620, 20),
+                         randrange(20, 460, 20))
+
+
+class Apple(GameObject):
+    """Описание класса для яблока"""
+
+    def __init__(self):
+        super().__init__()
+        self.body_color = APPLE_COLOR
+
 
 class Snake(GameObject):
+    """Определение для класса змеи"""
+
     def __init__(self):
         super().__init__()
         self.length = 2
@@ -83,35 +80,30 @@ class Snake(GameObject):
         self.body_color = SNAKE_COLOR
         self.last = None
 
-    # Метод обновления направления после нажатия на кнопку
     def update_direction(self):
+        """Метод обновления направления после нажатия на кнопку"""
         if self.next_direction:
             self.direction = self.next_direction
             self.next_direction = None
 
     def move(self):
-        # определение координаты по х:
+        """Метод, описывающий движение змейки"""
+        # # определение координаты по х:
         temporary_x_value = (
             self.positions[0][0] + self.direction[0] * GRID_SIZE
         )
-        if temporary_x_value > SCREEN_WIDTH:
-            temporary_x_value -= SCREEN_WIDTH
-        if temporary_x_value < 0:
-            temporary_x_value += SCREEN_WIDTH
         # определение координаты по у:
         temporary_y_value = (
             self.positions[0][1] + self.direction[1] * GRID_SIZE
         )
-        if temporary_y_value > SCREEN_HEIGHT:
-            temporary_y_value -= SCREEN_HEIGHT
-        if temporary_y_value < 0:
-            temporary_y_value += SCREEN_HEIGHT
-        self.positions.insert(0, (temporary_x_value, temporary_y_value))
+        self.positions.insert(0, (temporary_x_value % SCREEN_WIDTH,
+                                  temporary_y_value % SCREEN_HEIGHT))
         if len(self.positions) > int(self.length):
             list.pop(self.positions)
 
-    # Метод draw класса Snake
+    # Метод draw - переобределение для класса Snake
     def draw(self):
+        """Метод прорисовки для объекта"""
         for position in self.positions[:-1]:
             rect = (pygame.Rect(position, (GRID_SIZE, GRID_SIZE)))
             pygame.draw.rect(screen, self.body_color, rect)
@@ -127,77 +119,53 @@ class Snake(GameObject):
             last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
             pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
 
-    # не понимаю, для чего нужен метод ;(
+    # не понимаю, для чего нужно использовать метод, обошелся без него ;(
     def get_head_position(self):
+        """Не использовался"""
         return self.positions[0]
 
     def reset(self):
+        """Метод для возвращения к начальным параметрам"""
         self.length = 2
-        self.positions.clear()
         self.positions = [self.position, ]
         directions = (UP, DOWN, LEFT, RIGHT)
         self.direction = choice(directions)
 
 
 class Bluererry(GameObject):
+    """Описание класса для черники"""
+
     def __init__(self):
         super().__init__()
         self.body_color = BLUEBERRY_COLOR
 
-    def randomize_position(self):
-        self.position = (randint(1, 31) * GRID_SIZE,
-                         randint(1, 23) * GRID_SIZE)
-
-    def draw(self):
-        rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
-        pygame.draw.rect(screen, self.body_color, rect)
-        pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
-
 
 class Stone(GameObject):
+    """Описание класса для камня"""
+
     def __init__(self):
         super().__init__()
         self.body_color = STONE_COLOR
 
-    def randomize_position(self):
-        self.position = (randint(1, 31) * GRID_SIZE,
-                         randint(1, 23) * GRID_SIZE)
-
-    def draw(self):
-        rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
-        pygame.draw.rect(screen, self.body_color, rect)
-        pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
-
     def upgrade(self, length):
-        if length == 5 or length == 9:
-            return True
-        else:
-            return False
+        """Метод определения условия появления камня"""
+        return length == 5 or length == 9
 
 
 class Orange(GameObject):
+    """Описание класса для апельсина"""
+
     def __init__(self):
         super().__init__()
         self.body_color = ORANGE_COLOR
 
-    def randomize_position(self):
-        self.position = (randint(1, 31) * GRID_SIZE,
-                         randint(1, 23) * GRID_SIZE)
-
-    def draw(self):
-        rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
-        pygame.draw.rect(screen, self.body_color, rect)
-        pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
-
     def upgrade(self, length):
-        if length == 3 or length == 7:
-            return True
-        else:
-            return False
+        """Метод определения условия появления апельсина"""
+        return length == 3 or length == 7
 
 
-# Функция обработки действий пользователя
 def handle_keys(game_object):
+    """Функция обработки действий пользователя"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -213,21 +181,31 @@ def handle_keys(game_object):
                 game_object.next_direction = RIGHT
 
 
+apple = Apple()
+apple.randomize_position()
+snake = Snake()
+orange = Orange()
+orange.randomize_position()
+bluererry = Bluererry()
+bluererry.randomize_position()
+stone = Stone()
+stone.randomize_position()
+
+
+def snake_eating_itself():
+    """событие - змейка скушала себя"""
+    for i in range(1, len(snake.positions) - 1):
+        if snake.positions[i] == snake.positions[0]:
+            snake.reset()
+
+
 def main():
+    """Главная функция"""
     # Инициализация PyGame:
     pygame.init()
-    # поздаем переменную от константы для изменения скорости от апельсинки
+    # cоздаем переменную от константы для изменения скорости от апельсинки
     speed = SPEED
-    # Тут нужно создать экземпляры классов.
-    apple = Apple()
-    apple.randomize_position()
-    snake = Snake()
-    orange = Orange()
-    orange.randomize_position()
-    bluererry = Bluererry()
-    bluererry.randomize_position()
-    stone = Stone()
-    stone.randomize_position()
+    # запускаем цикл игры
     while True:
         clock.tick(speed)
         apple.draw()
@@ -259,11 +237,12 @@ def main():
         # событие - змейка съела камень
         if stone.position == snake.positions[0]:
             snake.reset()
+            speed = SPEED
         # событие - змейка съела себя:
         if len(snake.positions) >= 3:
-            for i in range(1, len(snake.positions) - 1):
-                if snake.positions[i] == snake.positions[0]:
-                    snake.reset()
+            snake_eating_itself
+            speed = SPEED
+
         pygame.display.update()
         screen.fill(BOARD_BACKGROUND_COLOR)
 
